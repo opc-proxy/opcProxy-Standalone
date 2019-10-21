ce7a228f0fbd1f2a3d15e6b6d3c15359afa4d65c
panManfredini <pan.manfredini@gmail.com>
2019-10-21 15:50:53 +0200

    first commit:

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using OpcProxyCore;
+using Newtonsoft.Json.Linq;
+using OpcProxyClient;
+using OpcGrpcConnect;
+using OpcInfluxConnect;
+
+
+namespace OPC_Proxy
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            serviceManager man = new serviceManager(args);            
+
+            HttpImpl opcHttpConnector = new HttpImpl();
+            InfluxImpl influx = new InfluxImpl();
+
+            man.addConnector(opcHttpConnector);
+            man.addConnector(influx);
+
+            man.run();
+
+            return (int)OPCclient.ExitCode;           
+
+            // db.Dispose();
+        }
+        
+    }
+}
